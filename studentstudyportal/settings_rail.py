f69b8983e448f01619910c0e76b0928cd98d4990
Chandrakant008 <140528001+Chandrakant008@users.noreply.github.com>
2025-07-04 21:49:08 +0530

    Fix: removed ellipsis from MIDDLEWARE list

--- studentstudyportal/settings_rail.py
+++ studentstudyportal/settings_rail.py
@@ -24,6 +24,11 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Add WhiteNoise middleware to serve static files
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',  # Add here
-    ...
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # This must be second
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
